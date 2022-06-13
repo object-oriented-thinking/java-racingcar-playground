6d77b4ceb84c58c307e5bf417e143ec9e5f97a5e
tis <92219795+this-is-spear@users.noreply.github.com>
2022-06-13 09:27:14 +0900

    refact : RacingCar 상태 행위 변경

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,21 +3,41 @@ package racingcar;
 import java.util.Random;
 
 public class RacingCar {
-    private static final int RANDOM_INTEGER = new Random().nextInt(10);
+    private static final Random RANDOM = new Random();
+
+    private final String name;
     private int distance;
 
-    public RacingCar() {
+    public RacingCar(String name) {
+        this.name = name;
         this.distance = 0;
+
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getDistance() {
         return distance;
     }
 
-    public void decideGoOrNot() {
-        if (RANDOM_INTEGER >= 4) {
+    public void racing() {
+        if (condition()) {
             go();
         }
+    }
+
+    public void printRacingResult() {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < distance; i++) {
+            stringBuffer.append("-");
+        }
+        System.out.printf("%s : %s", name, stringBuffer);
+    }
+
+    public boolean condition() {
+        return RANDOM.nextInt(10) >= 4;
     }
 
     public void go() {
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,44 +1,102 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RacingCarTest {
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
     @Test
     @DisplayName("처음 생성한 자동차의 이동 거리는 0이다.")
     void test1() {
-        RacingCar racingCar = new RacingCar();
-        assertThat(racingCar.getDistance()).isEqualTo(0);
+        String carName = "car";
+        RacingCar racingCar = new RacingCar(carName);
+        assertAll(
+            () -> assertThat(racingCar.getDistance()).isEqualTo(0),
+            () -> assertThat(racingCar.getName()).isEqualTo(carName)
+        );
     }
 
     @Test
     @DisplayName("자동차는 전진할 수 있다.")
     void test2() {
         //given
-        RacingCar racingCar = new RacingCar();
-        //when
-        racingCar.go();
-        //then
-        assertThat(racingCar.getDistance()).isEqualTo(1);
+        RacingCar racingCar = new RacingCar("car");
+        assertAll(
+            () -> {
+                //when
+                racingCar.go();
+                //then
+                assertThat(racingCar.getDistance()).isEqualTo(1);
+            },
+            () -> {
+                //when
+                racingCar.go();
+                //then
+                assertThat(racingCar.getDistance()).isEqualTo(2);
+            },
+            () -> {
+                //when
+                racingCar.go();
+                //then
+                assertThat(racingCar.getDistance()).isEqualTo(3);
+            }
+        );
     }
 
     @Test
     @DisplayName("전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4이상일 경우이다.")
     void test3() {
-        //given
-        RacingCar racingCar = new RacingCar();
-        //when & then
-        racingCar.decideGoOrNot();
-        assertThat(racingCar.getDistance()).isBetween(0, 1);
-        //when & then
-        racingCar.decideGoOrNot();
-        assertThat(racingCar.getDistance()).isBetween(0, 2);
-        //when & then
-        racingCar.decideGoOrNot();
-        System.out.println((racingCar.getDistance()));
-        assertThat(racingCar.getDistance()).isBetween(0, 3);
 
+        //given
+        RacingCar racingCar = new RacingCar("car");
+
+        assertAll(
+            () -> {
+                //when
+                racingCar.racing();
+                //then
+                assertThat(racingCar.getDistance()).isBetween(0, 1);
+            },
+            () -> {
+                //when
+                racingCar.racing();
+                //then
+                assertThat(racingCar.getDistance()).isBetween(0, 2);
+            },
+            () -> {
+                //when
+                racingCar.racing();
+                //then
+                assertThat(racingCar.getDistance()).isBetween(0, 3);
+            }
+        );
     }
+
+    @Test
+    @DisplayName("움직인 결과를 출력한다.")
+    void test4() {
+        //given
+        RacingCar racingCar = new RacingCar("name");
+        //when
+        racingCar.go();
+        racingCar.go();
+        racingCar.printRacingResult();
+        //then
+        assertThat("name : --").isEqualTo(outputStreamCaptor.toString().trim());
+    }
+
 }
