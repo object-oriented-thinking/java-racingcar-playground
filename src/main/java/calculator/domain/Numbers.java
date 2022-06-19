3d664f84658cbe191282ddf3aed1c0297c6827e0
oliviarla <imoliviarla@gmail.com>
2022-06-19 22:09:14 +0900

    refactor: 불변 리스트 구현, 접근제어자 수정, 중복제거

--- src/main/java/calculator/domain/Numbers.java
+++ src/main/java/calculator/domain/Numbers.java
@@ -1,20 +1,21 @@
 package calculator.domain;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Numbers {
-    private final List<Integer> numbers ;
+    private final List<Integer> numbers;
 
     public Numbers(String[] strings) {
-        if(Arrays.stream(strings).filter(s -> !s.isEmpty() && s.chars().anyMatch(Character::isDigit)).map(Integer::parseInt).anyMatch(integer -> integer<0)){
+        if(Arrays.stream(strings).filter(s -> !s.trim().isEmpty() && s.chars().anyMatch(Character::isDigit)).map(Integer::parseInt).anyMatch(integer -> integer<0)){
             throw new RuntimeException("음수가 입력되었습니다.");
         }
-        this.numbers = Arrays.stream(strings).filter(s -> !s.isEmpty() && s.chars().allMatch(Character::isDigit)).map(Integer::parseInt).filter(integer -> integer>=0).collect(Collectors.toList());
+        this.numbers = Arrays.stream(strings).filter(s -> !s.trim().isEmpty()).map(Integer::parseInt).collect(Collectors.toList());
     }
 
-    public final List<Integer> getNumbers() {
-        return numbers;
+    public  List<Integer> getNumbers() {
+        return numbers.stream().collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
     }
 }
